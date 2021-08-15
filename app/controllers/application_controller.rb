91f3e2667a08a5701c53b7063e54fa659363d55e
taka61 <taka.tennisloveup61@gmail.com>
2021-08-15 13:03:36 +0900

    カミナリをインストールしページングを実装しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def index
+    @books = Book.page(params[:page]).per(5).order(:created_at, :id).to_sql
+  end
 end
