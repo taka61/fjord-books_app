7501f77bfae3d2aea146800c6b33eb1e20ad3d7a
taka61 <taka.tennisloveup61@gmail.com>
2021-08-18 17:49:48 +0900

    再途中経過

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
+
 end
