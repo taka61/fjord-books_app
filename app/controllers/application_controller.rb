7bfb6879c5aea130e659d197804acc095eb31193
taka61 <taka.tennisloveup61@gmail.com>
2021-08-19 18:17:38 +0900

    ページ当たりで表示する数値をデフォルト値に戻しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def index
-    @books = Book.page(params[:page]).per(5).order(:created_at, :id).to_sql
-  end
 end
