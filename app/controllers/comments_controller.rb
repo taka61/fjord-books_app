e48e80dfec7356003da9e7e10339f7d12dca3d9e
taka61 <taka.tennisloveup61@gmail.com>
2021-09-03 18:03:54 +0900

    コメントをつけるの課題提出

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to @commentable
+  end
+
+  def destroy
+    @comment = @commentable.comments.find_by(params[:id])
+    @comment.destroy
+    redirect_to @commentable
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+  resources :books do
+    resources :comments, module: :books
+  end
+  resources :reports do
+    resources :comments, module: :reports
+  end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
